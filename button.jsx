dc0c5381ff289936f4332f99d1c650f0ee9ea5a4
AntiFinman <177669580+AntiFinman@users.noreply.github.com>
2024-08-20 14:38:23 +0100

    made a button in jsx

---
+++ button.jsx
@@ -0,0 +1,12 @@
+class name {
+    constructor(parameters) {
+        <Button
+          onPress={onPressLearnMore}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+
+        
+    }
+}
